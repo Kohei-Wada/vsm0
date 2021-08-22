6c3e8a71e1f07a1612fceebafcce56b87cea1481
Kohei-Wada <program3152019@gmail.com>
2021-08-22 20:08:02 +0900

    added method to vsm.c

--- vsm.c
+++ vsm.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct vsm {
+	int i;
+} vsm_t;
+
+
+
+void vsm_init(vsm_t **v)
+{
+	(*v) = malloc(sizeof(vsm_t));
+}
+
+
+void vsm_free(vsm_t *v)
+{
+	free(v);
+}
+
 
 int main(void) 
 {
+	vsm_t *vsm;
+
+	vsm_init(&vsm);
+	vsm_free(vsm);
 
 	return 0;
 }
